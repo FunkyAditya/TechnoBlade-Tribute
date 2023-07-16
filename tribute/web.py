b9c08b20041c2316dc38642be10d43074e66db32
Aditya Prakash <114856260+FunkyAditya@users.noreply.github.com>
2023-07-17 00:04:00 +0530

    Update web.py

--- tribute/web.py
+++ tribute/web.py
@@ -1,19 +1,1 @@
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def new():
-    return '''<html>
-    <head>
-        <title>Hello</title>
-        <link href="hello.css" rel="stylesheet" type="text/css" >
-    </head>
-    <body><center><button class="button"><a href="new.html">Click To See Tribute For TechnoBlade</a></button>
-    </center>
-        
-    </body>
-</html>'''
-
-if __name__ == "__main__":
-    app.run()
